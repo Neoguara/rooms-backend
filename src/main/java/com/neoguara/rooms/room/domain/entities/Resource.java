87e00cc6f77e04a6b24689c27aa0c3cee37fa597
JuniorJose-git <joseronaldosantus@gmail.com>
2026-05-10 14:17:44 -0300

    .

--- src/main/java/com/neoguara/rooms/room/domain/entities/Resource.java
+++ src/main/java/com/neoguara/rooms/room/domain/entities/Resource.java
@@ -1,6 +1,8 @@
 package com.neoguara.rooms.room.domain.entities;
 
+import com.neoguara.rooms.room.domain.validation.ResourceValidation;
 import com.neoguara.rooms.room.domain.valueobjects.ResourceId;
+import com.neoguara.rooms.shared.domain.validation.Notification;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -16,9 +18,52 @@ public class Resource {
     private String name;
     private String description;
     private String icon;
-    private Boolean isActive;
+    private Boolean active;
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
+    Resource () {}
+
+    private Resource(String name, String description, String icon) {
+        this.id = new ResourceId();
+        this.name = name;
+        this.description = description;
+        this.icon = icon;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public static Resource create(String name, String description, String icon) {
+        Resource resource = new Resource(name, description, icon);
+        Notification notification = Notification.create();
+        new ResourceValidation().validate(resource, notification);
+        notification.raiseIfHasErrors();
+        return resource;
+    }
+
+    public void update(String name, String description, String icon) {
+        this.name = name;
+        this.description = description;
+        this.icon = icon;
+        Notification notification = Notification.create();
+        new ResourceValidation().validate(this, notification);
+        notification.raiseIfHasErrors();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void activate() {
+        this.active = true;
+    }
+
+    public void deactivate() {
+        this.active = false;
+    }
+
+    public ResourceId getId() {return id;}
+    public String getName() {return name;}
+    public String getDescription() {return description;}
+    public String getIcon() {return icon;}
+    public Boolean getActive() {return active;}
+    public LocalDateTime getCreatedAt() {return createdAt;}
+    public LocalDateTime getUpdatedAt() {return updatedAt;}
 }
---
+++ src/main/java/com/neoguara/rooms/room/domain/validation/ResourceValidation.java
@@ -0,0 +1,12 @@
+package com.neoguara.rooms.room.domain.validation;
+
+import com.neoguara.rooms.room.domain.entities.Resource;
+import com.neoguara.rooms.shared.domain.validation.Notification;
+import com.neoguara.rooms.shared.domain.validation.Validator;
+
+public class ResourceValidation implements Validator<Resource> {
+    @Override
+    public void validate(Resource target, Notification notification) {
+
+    }
+}
