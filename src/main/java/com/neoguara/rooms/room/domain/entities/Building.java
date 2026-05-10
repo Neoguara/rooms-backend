0c4a6a16c53e98e099821f5fa8da8ed45d1dffa8
JuniorJose-git <joseronaldosantus@gmail.com>
2026-05-10 01:15:41 -0300

    .

--- src/main/java/com/neoguara/rooms/room/domain/entities/Building.java
+++ src/main/java/com/neoguara/rooms/room/domain/entities/Building.java
@@ -1,5 +1,6 @@
 package com.neoguara.rooms.room.domain.entities;
 
+import com.neoguara.rooms.room.domain.enums.BuildingStatus;
 import com.neoguara.rooms.room.domain.valueobjects.BuildingId;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -17,10 +18,42 @@ public class Building {
     private String name;
     private String address;
     private Integer totalFloors;
-    private Boolean IsActive;
+    private BuildingStatus status;
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    protected Building() {}
+    Building() {}
+
+    public void activate () {
+        if (this.status == BuildingStatus.ARCHIVED) {
+            throw new Exception("Archived building cannot be activated");
+        }
+        this.status = BuildingStatus.ACTIVE;
+    }
+
+    public void deactivate () {
+        if (status == BuildingStatus.ARCHIVED) {
+//            throw new BusinessException("Archived building cannot be deactivated");
+        }
+        this.status = BuildingStatus.INACTIVE;
+    }
+
+    public void archive() {
+        this.status = BuildingStatus.ARCHIVED;
+    }
+
+    public void restore () {
+        if (status != BuildingStatus.ARCHIVED) {
+
+        }
+        this.status = BuildingStatus.ACTIVE;
+    }
+
+    public boolean isAvailable() {
+        return status == BuildingStatus.ACTIVE;
+    }
+
+
+
 }
