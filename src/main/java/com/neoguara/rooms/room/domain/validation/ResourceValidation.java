2421d36451511009fe2b46c91cbb30dc566753bc
JuniorJose-git <joseronaldosantus@gmail.com>
2026-05-10 14:20:20 -0300

    .

--- src/main/java/com/neoguara/rooms/room/domain/validation/ResourceValidation.java
+++ src/main/java/com/neoguara/rooms/room/domain/validation/ResourceValidation.java
@@ -7,6 +7,9 @@ import com.neoguara.rooms.shared.domain.validation.Validator;
 public class ResourceValidation implements Validator<Resource> {
     @Override
     public void validate(Resource target, Notification notification) {
-
+        notification
+                .addErrorIf(target.getName() == null || target.getName().isBlank(), "name is required")
+                .addErrorIf(target.getDescription() == null || target.getDescription().isBlank(), "description is required")
+                .addErrorIf(target.getIcon() == null || target.getIcon().isBlank(), "icon is required");
     }
 }
